d408f2598f679d664b982ca6384c9d684b2746f9
Angel Darco <angel92-eme@hotmail.com>
2023-11-25 11:38:41 -0500

    :fire: PreviewViewer module renamed to Viewer

--- src/component/ImageContainer.tsx
+++
@@ -1,6 +0,0 @@
-import { CustomImageViewerProps } from "../types/index.module";
-
-const ImageContainer: React.FC<CustomImageViewerProps> = (props) => {
-  return <img {...props} />;
-};
-export default ImageContainer;
