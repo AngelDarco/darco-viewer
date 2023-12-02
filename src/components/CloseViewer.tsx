3f371efea8d9851ba868dd3f85a1bd387b72aa34
Angel Darco <angel92-eme@hotmail.com>
2023-12-02 12:26:21 -0500

    :lipstick: close icon styled

---
+++ src/components/CloseViewer.tsx
@@ -0,0 +1,28 @@
+const CloseViewer = ({
+  controls,
+  close,
+}: {
+  controls: boolean;
+  close: () => void;
+}) => {
+  return (
+    <div
+      className={`
+    ${!controls ? "opacity-40" : "backdrop-opacity-100"}
+    `}
+      onClick={() => close()}
+    >
+      <svg
+        stroke="currentColor"
+        fill="currentColor"
+        strokeWidth="0"
+        viewBox="0 0 512 512"
+        height="100%"
+        width="100%"
+      >
+        <path d="M405 136.798L375.202 107 256 226.202 136.798 107 107 136.798 226.202 256 107 375.202 136.798 405 256 285.798 375.202 405 405 375.202 285.798 256z"></path>
+      </svg>
+    </div>
+  );
+};
+export default CloseViewer;
